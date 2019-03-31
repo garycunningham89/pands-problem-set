d91a973b8c3297bdaac8b4d0dd4f291fecf9cae8
Gary Cunningham <garycunningham1989@outlook.com>
2019-03-31 11:06:06 +0100

    Solution8Edit

--- solution8datetime.py
+++ solution8datetime.py
@@ -2,31 +2,35 @@
 #My program intends to output the date and time in the format Day, Month Date Year at Time(am/pm).
 #Adaptation from python tutorials, class content and w3schools.com interactive tutorials.
 
-import datetime
-# Called datetime as learned in tutorials and previous solution.
-a = datetime.datetime.now().strftime("%A, %B")
-b = datetime.datetime.now().strftime("%d")
-c = datetime.datetime.now().strftime("%Y at %H:%M%p")
-# Utilized datetime.datetime.now() as previous but had to consult the python.org
-# directive table of datetime inputs to ensure accurate output as per problem set.
 
 def ordinal(num):
     # Trying to create my own function for the ordinal numbers for all possibilities in a month.
-        if num == 1 or num == 21 or num == 31:
-            print(num + "st")
-            # To get 1st, 21st, 31st.
-        elif num == 2 or num == 22:
-            print(num + "nd")
-            # To get 2nd, 22nd.
-        elif num == 3 or num == 23:
-            print(num + "rd")
-            #To get 3rd, 23rd.
-        else:
-            print(num + "th")
-            # All else between 1 - 31 except above end with "th".
-        return(str(num))
+
+    if num == 1 or 21 or 31:
+        print(num + "st")
+        # To get 1st, 21st, 31st.
+    elif num == 2 or 22:
+        print(num + "nd")
+        # To get 2nd, 22nd.
+    elif num == 3 or 23:
+        print(num + "rd")
+        #To get 3rd, 23rd.
+    else:
+        print(num + "th")
+        # All else between 1 - 31 except above end with "th".
+    return(str(num))
       
-print(a, end =" ") 
-ordinal(b) 
+import datetime
+date = datetime.datetime.now()
+# Called datetime as learned in tutorials and previous solution.
+    
+a = date.strftime("%A, %B")
+b = date.strftime("%d")
+c = date.strftime("%Y at %H:%M%p")
+# Utilized datetime.datetime.now() as previous but had to consult the python.org
+# for the directive table of datetime inputs to ensure accurate output as per problem set.
+print(a, end = " ")
+ordinal(b)
 print(c)
-#Calling function as well as datetimes surrounding it to get the correct datetime format and output.
+#Calling function as well as datetimes surrounding it to get the correct datetime format and output as close as possible.
+#Found difficulty in combining the output on one line as the ordinal() was returning errors as Nonetype and would not allow end = "" or be printed accurately.
